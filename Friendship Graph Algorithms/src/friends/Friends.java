74848f9a7b831c689107b454f1f175796c5a006a
Ajay Srivastava <ajaysriv3@gmail.com>
2015-04-26 11:09:20 -0400

    Added sample graph file, deleted FriendsDriver

--- Friendship Graph Algorithms/src/friends/Friends.java
+++ Friendship Graph Algorithms/src/friends/Friends.java
@@ -1,11 +1,28 @@
 package friends;
 
-import java.lang.*;
 import java.io.*;
 import java.util.*;
-
+import java.lang.*;
 
 public class Friends
 {
-	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the friends graph file: ");
+		String graphFile = sc.next();
+		
+		//build graph
+		
+		while (true)
+		{
+			System.out.println("\n(1) Shortest Intro Chain");
+			System.out.println("(2) Cliques at School");
+			System.out.println("(3) Connectors");
+			System.out.println("(4) Quit");
+			System.out.print("Enter Option --> ");
+			int option = sc.nextInt();
+		}
+
+	}
 }
